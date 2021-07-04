54d09497461c0336e37abdee4b3cebed6c89db9d
Gittingthehubbing <81262964+Gittingthehubbing@users.noreply.github.com>
2021-07-04 09:47:13 +0100

    Improved hyperparameter optimization

--- Tabular Playground Series - Jan 2021/tabJan21.py
+++ Tabular Playground Series - Jan 2021/tabJan21.py
@@ -21,10 +21,13 @@ import seaborn as sb
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor as rfr
 from sklearn.linear_model import BayesianRidge as bayR
+from sklearn.experimental import enable_hist_gradient_boosting
+from sklearn.ensemble import HistGradientBoostingRegressor as gradB
 from sklearn.preprocessing import StandardScaler as ss
 from sklearn.model_selection import train_test_split as tts
 from sklearn.preprocessing import StandardScaler as ss
 from sklearn.metrics import mean_squared_error as mse
+from joblib import parallel_backend # to improve sklearn training speed
 import os
 import datetime
 from tensorboardX import SummaryWriter
@@ -120,8 +123,8 @@ class net(nn.Module):
 
 
 class dset(Dataset):
-    
-    
+
+
     def __init__(self, xAll,yAll):
         super().__init__()
         self.xAll = xAll
@@ -143,34 +146,82 @@ def makeModel(trial):
     layers=[]
     inFeat = xTrT.shape[1] #assumes 1D data for each sample
     lastOut = yTrT.shape[1]
-    
+
     for i in range(nl):
         outFeat = trial.suggest_int(f"n{i}",minNeurons,maxNeurons)
-        layers.append(nn.Linear(inFeat, outFeat)) 
-        actiLayer = trial.suggest_categorical(f"a{i}",possibleActiFuncs) 
+        layers.append(nn.Linear(inFeat, outFeat))
+        actiLayer = trial.suggest_categorical(f"a{i}",possibleActiFuncs)
         layers.append(getattr(nn, actiLayer)())
         dropOutRatio = trial.suggest_float(f"dropoutL{i}",minDropOut,maxDropOut)
-        layers.append(nn.Dropout(dropOutRatio))        
-        inFeat = outFeat 
+        layers.append(nn.Dropout(dropOutRatio))
+        inFeat = outFeat
     layers.append(nn.Linear(inFeat, lastOut))
-    
+
     return nn.Sequential(*layers)
-        
+
 def shallowObjective(trial):
-    
-    model = trial.suggest_categorical("modelname",possibleShallows)
+
+    modelname = trial.suggest_categorical("modelname",possibleShallows)
+    idxArr = np.arange(len(xTrT))
+    np.random.shuffle(idxArr)
+    idxSel = idxArr[:numShallowSamples]
+    if modelname == "SVR":
+        C = trial.suggest_float("C",1e0,1e4,log=True)
+        kernel = trial.suggest_categorical("kernel", ["linear", "poly", "rbf", "sigmoid"])
+        if kernel == 'poly':
+            polyDeg = trial.suggest_int("polyDeg",2,6)
+        else:
+            polyDeg = 3
+        if kernel in ['rbf','poly','sigmoid']:
+            gamma = trial.suggest_categorical('gamma',['scale', 'auto'])
+        else:
+            gamma = 'scale'
+        if kernel in ['poly','sigmoid']:
+            coef0 = trial.suggest_float('coef0',0.,0.2)
+        else:
+            coef0 = 0.
+        model = SVR(kernel=kernel, degree=polyDeg, gamma=gamma, coef0=coef0, C=C,verbose=1)
+        with parallel_backend('threading', n_jobs=-1):
+            model.fit(xTrT[idxSel], yTrT[idxSel].reshape(-1))
+            trainLoss = mse(yTrT[idxSel].reshape(-1),model.predict(xTrT[idxSel]).reshape(-1))
+            evalLoss = mse(yET.reshape(-1),model.predict(xET).reshape(-1))
+            print(modelname,f' TrainLoss {trainLoss:.3f}, evalLoss: {evalLoss:.3f}')
+
+    elif modelname == "rfr":
+        n_estimators = trial.suggest_int("n_estimators",10,1000)
+        max_features = trial.suggest_categorical("max_features",["sqrt","log2",None])
+        max_depth = trial.suggest_int("max_depth",10,100)
+        model = rfr(n_estimators=n_estimators,max_features=max_features,max_depth=max_depth,verbose=1)
+        with parallel_backend('threading', n_jobs=-1):
+            model.fit(xTrT[idxSel], yTrT[idxSel].reshape(-1))
+            trainLoss = mse(yTrT[idxSel].reshape(-1),model.predict(xTrT[idxSel]).reshape(-1))
+            evalLoss = mse(yET.reshape(-1),model.predict(xET).reshape(-1))
+            print(modelname,f' TrainLoss {trainLoss:.3f}, evalLoss: {evalLoss:.3f}')
+
+    elif modelname == "gradB":
+        learning_rate = trial.suggest_float("learning_rate",1e-4,1e0,log=True)
+        max_leaf_nodes = trial.suggest_int("max_leaf_nodes",1,500)
+        max_depth = trial.suggest_int("max_depth",10,100)
+        model = gradB(learning_rate=learning_rate,max_leaf_nodes=max_leaf_nodes,max_depth=max_depth,verbose=1)
+        with parallel_backend('threading', n_jobs=-1):
+            model.fit(xTrT[idxSel], yTrT[idxSel].reshape(-1))
+            trainLoss = mse(yTrT[idxSel].reshape(-1),model.predict(xTrT[idxSel]).reshape(-1))
+            evalLoss = mse(yET.reshape(-1),model.predict(xET).reshape(-1))
+            print(modelname,f' TrainLoss {trainLoss:.3f}, evalLoss: {evalLoss:.3f}')
+    return evalLoss
+
 
 def objective(trial):
-    
+
     model = makeModel(trial).to(device)
     lr = trial.suggest_float("lr", minLr, maxLr, log=True)
-    
+
     opti = t.optim.Adam(model.parameters(),lr = lr)
     evalLossMean = trainNN(model,opti,trDl, evalDl,epochs=epochs, trial=trial)
     return evalLossMean
 
 def trainNN(nnNet,opti,trDl, evalDl,epochs=2, trial=None):
-    
+
     writerCount=0
     evalWriterCount=0
     trLosses =[]
@@ -198,7 +249,7 @@ def trainNN(nnNet,opti,trDl, evalDl,epochs=2, trial=None):
                 writerTr.add_scalar("lossTr", lossTr.cpu().detach().numpy().item(),writerCount)
             if trial is not None and i >= numBatchesForOptunaTr*batchSize:
                 break
-                
+
 
             if i%500==0:
                 with t.no_grad():
@@ -219,7 +270,7 @@ def trainNN(nnNet,opti,trDl, evalDl,epochs=2, trial=None):
                             break
         trLosses.append(np.mean(epochLossTr))
         evalLosses.append(np.mean(epochLossE))
-        
+
         if trial is not None:
             trial.report(np.mean(epochLossE), e)
             if trial.should_prune():
@@ -242,23 +293,23 @@ def trainNN(nnNet,opti,trDl, evalDl,epochs=2, trial=None):
 mainDir = r"E:\KaggleData\Tabular Playground Series - Jan 2021"
 
 #Hyperparameters and toggles
-epochs = 25
+epochs = 50
 lr = 6e-6
 batchSize = 1024
 addL1Reg = True
 L1val = 0.004
-doLearningCurve = True
+doLearningCurve = False
 
 logTB = 0
 
 doShallows =False
-doPYNN = 0
+doPYNN = True
 doLightning = 0
 tuneModel = 0
 
 doOptuna = True
 #optuna Params
-possibleActiFuncs = ["ReLU","Sigmoid","LeakyReLU","Tanh"]
+possibleActiFuncs = ["ReLU"] #,"Sigmoid","LeakyReLU","Tanh"
 minLayers = 1
 maxLayers = 6
 minNeurons = 20
@@ -269,13 +320,14 @@ l1min = 1e-7
 l1max = 1e-2
 minLr = 1e-8
 maxLr = 1e-3
-maxTrials = 5
+maxTrials = 100
 maxTime = 60000
 numBatchesForOptunaTr = 30
-numBatchesForOptunaTe = 10
+numBatchesForOptunaTe = 20
 
 #optuna shallow params
-possibleShallows = ["SVR","rfr","bayR"]
+possibleShallows = ["rfr","gradB"]#,"SVR","bayR","gradB"
+numShallowSamples = 50000
 
 #Toggles for data overview
 doPCA = False #looks to be unhelpful
@@ -285,7 +337,7 @@ plotCorreclations = False
 
 os.chdir(mainDir)
 dateTimeNow =  datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-if logTB:    
+if logTB:
     writerTr = SummaryWriter("logs/"+dateTimeNow+"TrainLosses")
     writerEval = SummaryWriter("logs/"+dateTimeNow+"EvalLosses")
 
@@ -308,10 +360,10 @@ if doPCA:
         pcaPlot = PCA(n_components=3).fit(trainDataRaw.drop([targetCol, idCol],axis=1), y=trTarget)
         xPCAplot = pcaPlot.transform(trainDataRaw.drop([targetCol, idCol],axis=1))
         yPCAplot = trTarget.copy()
-        
-        
+
+
         #%matplotlib qt
-        
+
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
         numSamples = 1000
@@ -321,10 +373,10 @@ if doPCA:
         ax.scatter(xPCAplot[idxSel,0], xPCAplot[idxSel,1], xPCAplot[idxSel,2], c = yPCAplot[idxSel])
         plt.show()
         plt.close()
-        
+
         #%matplotlib inline
-        
-            
+
+
 if plotCorreclations:
     corrs = trainDataRaw.drop("id",axis=1).corr()
     corrPlotThreshold = 1 # to check for weak correlations, cont11 and 12 correlate highly
@@ -374,13 +426,26 @@ evalDl = dl(dSetE,batch_size=batchSize,shuffle=True)
 # Shallow tests
 
 if doShallows:
-    ranForest = rfr(verbose=1) # mse: 0.130517
-    bay = bayR(verbose=1)
-    svr = SVR(verbose=1)
 
-    bay.fit(xTrT, yTrT)
-    bay = mse(bay.predict(xTrT),yTrT)
-    print('Forest MSE: ',np.sqrt(bay))
+    if doOptuna:
+        studyShallow = opt.create_study(direction="minimize")
+        studyShallow.optimize(shallowObjective, maxTrials)
+        shallowTrial = studyShallow.best_trial
+        print(shallowTrial)
+        with open(f'./Shallow_BestTrialParams{dateTimeNow}.txt','w+') as f:
+            for k, v in shallowTrial.params.items():
+                f.write(f"'{k}':{v}\n")
+                print(f"'{k}':{v},")
+    else:
+        ranForest = rfr(verbose=1) # mse: 0.130517
+        bay = bayR(verbose=1)
+        svr = SVR(verbose=1)
+
+        bay.fit(xTrT, yTrT)
+        bay = mse(bay.predict(xTrT),yTrT)
+        print('Forest MSE: ',np.sqrt(bay))
+
+
 
 
 # nn approach
@@ -388,14 +453,19 @@ device = t.device('cuda') if t.cuda.is_available() else t.device('cpu')
 writerCount = 0
 evalWriterCount = 0
 if doPYNN:
-    # nnNet = net(xTrT, yTrT).to(device)
-    # opti = t.optim.Adam(nnNet.parameters(), lr = lr)
-    # criterion = nn.MSELoss()
 
-    # evalLossFromTrain = trainNN(trDl, evalDl)
-    # print('Final Loss val ', evalLossFromTrain)
+    if doOptuna:
+        criterion = nn.MSELoss()
+        study = opt.create_study(direction="minimize")
+        study.optimize(objective, n_trials=maxTrials, timeout=maxTime)
+        trial = study.best_trial
+        print('\nBest Study Parameters:')
 
-    if doLearningCurve:
+        with open(f'./BestTrialParams{dateTimeNow}.txt','w+') as f:
+            for k, v in trial.params.items():
+                f.write(f"'{k}':{v}\n")
+                print(f"'{k}':{v},")
+    elif doLearningCurve:
         indsTr =np.arange(len(dSetTr))
         np.random.shuffle(indsTr)
         subTrDl = dl(Subset(dSetTr,indsTr[:50000]),batch_size=batchSize,shuffle=True)
@@ -407,62 +477,22 @@ if doPYNN:
         evalLossFromTrain = trainNN(subTrDl, evalDl, epochs=3)
         print('Final Loss val ', evalLossFromTrain)
 
-    # trLosses =[]
-    # evalLosses = []
-    # for e in tqdm(range(epochs)):
-    #     epochLossTr = []
-    #     epochLossE = []
-    #     for i, (x,y) in enumerate(trDl):
-    #         nnNet.train()
-    #         out = nnNet(x.to(device))
-    #         lossTr = criterion(y.to(device), out)
-    #         if addL1Reg:
-    #                 l2_reg = t.tensor(0.).to(device)
-    #                 for parameter in nnNet.parameters():
-    #                     l2_reg += t.linalg.norm(parameter)
-    #                 lossTr += L1val * l2_reg
-    #         opti.zero_grad()
-    #         lossTr.backward()
-    #         opti.step()
-    #         epochLossTr.append(lossTr.cpu().detach().numpy().item())
-    #         if logTB:
-    #             writerCount+=1
-    #             writerTr.add_scalar("lossTr", lossTr.cpu().detach().numpy().item(),writerCount)
+    else:
+        nnNet = net(xTrT, yTrT).to(device)
+        opti = t.optim.Adam(nnNet.parameters(), lr = lr)
+        criterion = nn.MSELoss()
 
+        evalLossFromTrain = trainNN(trDl, evalDl)
+        print('Final Loss val ', evalLossFromTrain)
 
-    #         if i%50==0:
-    #             with t.no_grad():
-    #                 for iTe,(xTe, yTe) in enumerate(evalDl):
-    #                     nnNet.eval()
-    #                     outTe = nnNet(xTe.to(device))
-    #                     lossTe = criterion(yTe.to(device),outTe)
-    #                     if addL1Reg:
-    #                         l2_reg = t.tensor(0.).to(device)
-    #                         for parameter in nnNet.parameters():
-    #                             l2_reg += t.linalg.norm(parameter)
-    #                         lossTe += L1val * l2_reg
-    #                     epochLossE.append(lossTe.cpu().detach().numpy().item())
-    #                     if logTB:
-    #                         evalWriterCount +=1
-    #                         writerEval.add_scalar("lossEval", lossTe.cpu().detach().numpy().item(),evalWriterCount)
-    #     trLosses.append(np.mean(epochLossTr))
-    #     evalLosses.append(np.mean(epochLossE))
-
-    # # plot losses
-
-    # plt.plot(trLosses, 'k.')
-    # plt.plot(evalLosses, 'r.')
-    # plt.show()
-    # plt.close()
-
-    nnNet.eval()
-    submissionY = nnNet(t.Tensor(teDT).to(device)).cpu().detach().numpy()
-    dfSubmission = pd.DataFrame(data=submissionY,
-                                index=np.arange(0,len(submissionY)))
-    dfSubmission.reset_index(inplace=True)
-    dfSubmission.columns = ['id','target']
-    # dfSubmission.rename({'index':'id'},inplace=True)
-    dfSubmission.to_csv(f'logs/{dateTimeNow}_Submission.csv',index=False)
+        nnNet.eval()
+        submissionY = nnNet(t.Tensor(teDT).to(device)).cpu().detach().numpy()
+        dfSubmission = pd.DataFrame(data=submissionY,
+                                    index=np.arange(0,len(submissionY)))
+        dfSubmission.reset_index(inplace=True)
+        dfSubmission.columns = ['id','target']
+        # dfSubmission.rename({'index':'id'},inplace=True)
+        dfSubmission.to_csv(f'logs/{dateTimeNow}_Submission.csv',index=False)
 
 ## nn with LightningModule
 if doLightning:
@@ -479,15 +509,3 @@ if doLightning:
         # trainer.tune(model1,trDl)
     else:
         trainer.fit(model1, trDl)
-        
-if doOptuna:
-    criterion = nn.MSELoss()
-    study = opt.create_study(direction="minimize")
-    study.optimize(objective, n_trials=maxTrials, timeout=maxTime)
-    trial = study.best_trial
-    print('\nBest Study Parameters:')
-    
-    with open(f'./BestTrialParams{dateTimeNow}.txt','w+') as f:
-        for k, v in trial.params.items():
-            f.write(f"'{k}':{v}\n")
-            print(f"'{k}':{v},")
